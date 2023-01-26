76a5dc7939371ec364d07985f06fbed92fa1da63
yar9bit <yar9bit@gmail.com>
2023-01-26 19:46:33 +0500

    Update tk.py

--- interface/tk.py
+++ interface/tk.py
@@ -2,13 +2,14 @@ from tkinter import *
 from tkinter import ttk
 from sample import list_sample
 from datetime import datetime
+import pyperclip
 
 root = Tk()
 root.title('Программа')
 root.geometry("640x640")
 
 frame = Frame(root)
-frame.pack(expand=False)
+frame.pack(expand=0)
 
 label = ttk.Label(frame, text="Программа", font="TimesNewRoman 14 bold")
 label.pack()
@@ -41,13 +42,27 @@ def result():
     try:
         res = cbox_item_select1('') - cbox_item_select2('')
         label['text'] = round(res.total_seconds())
+        return round(res.total_seconds())
     except ValueError:
         msg = 'Не выбраны значения'
         label["text"] = msg
 
 
-btn = ttk.Button(frame, text='Результат', command=result)
-btn.pack()
+def copy_result():
+    if result():
+        copy = pyperclip.copy(result())
+        label['text'] = 'Данные скопированы'
+    return copy
+
+
+frame_button = ttk.Frame(root)
+frame_button.pack(expand=0, anchor=CENTER)
+
+btn_result = ttk.Button(frame_button, text='Результат', command=result)
+btn_result.pack()
+
+btn_copy = ttk.Button(frame_button, text='Copy result', command=copy_result)
+btn_copy.pack()
 
 cbox1.bind("<<ComboboxSelected>>", cbox_item_select1)
 cbox2.bind("<<ComboboxSelected>>", cbox_item_select2)
