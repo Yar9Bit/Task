1c7c7027cde3490c00442081966e15ae336ed381
yar9bit <yar9bit@gmail.com>
2023-02-02 14:59:24 +0500

    Upd

--- interface/tk.py
+++ interface/tk.py
@@ -4,69 +4,64 @@ from sample import list_sample
 from datetime import datetime
 import pyperclip
 
-root = Tk()
-root.title('Программа')
-root.geometry("640x640")
 
-frame = Frame(root)
-frame.pack(expand=0)
+class App(Tk):
+    date_time = []
+    for i in list_sample:
+        date_time.append(i[1])
 
-label = ttk.Label(frame, text="Программа", font="TimesNewRoman 14 bold")
-label.pack()
+    def __init__(self):
+        super().__init__()
 
-date_time = []
-for i in list_sample:
-    date_time.append(i[1])
+        self.title('Программа')
+        self.geometry('640x640')
+        self.label = ttk.Label(self, text="Программа", font="TimesNewRoman 14 bold")
+        self.label.pack(anchor=CENTER)
 
-cbox1 = ttk.Combobox(frame, values=date_time)
-cbox1.pack(side=LEFT)
-cbox2 = ttk.Combobox(frame, values=date_time)
-cbox2.pack(side=RIGHT)
+        self.combobox1 = ttk.Combobox(self, values=self.date_time)
+        self.combobox1.bind("<<ComboboxSelected>>", self.combobox_value_select1)
+        self.combobox1.pack()
+
+        self.combobox2 = ttk.Combobox(self, values=self.date_time)
+        self.combobox2.bind("<<ComboboxSelected>>", self.combobox_value_select2)
+        self.combobox2.pack()
+
+        self.btn_result = ttk.Button(self, text='Результат', command=self.result)
+        self.btn_result.pack()
+
+        self.btn_copy_result = ttk.Button(self, text='Копировать результат', command=self.copy_result)
+        self.btn_copy_result.pack()
+
+    def combobox_value_select1(self, event):
+        selection = self.combobox1.get()
+        format_date = datetime.strptime(selection, '%d.%m.%Y %H:%M:%S')
+        print(format_date)
+        return format_date
+
+    def combobox_value_select2(self, event):
+        selection = self.combobox2.get()
+        format_date = datetime.strptime(selection, '%d.%m.%Y %H:%M:%S')
+        print(format_date)
+        return format_date
+
+    def result(self):
+        try:
+            res = self.combobox_value_select1('') - self.combobox_value_select2('')
+            self.label['text'] = round(res.total_seconds())
+            return round(res.total_seconds())
+        except ValueError:
+            msg = 'Не выбраны значения'
+            self.label["text"] = msg
+
+    def copy_result(self):
+        if self.result():
+            copy = pyperclip.copy(self.result())
+            self.label['text'] = 'Данные скопированы'
+            return copy
+        else:
+            self.label['text'] = 'Нет данных'
 
 
-def cbox_item_select1(event):
-    selection = cbox1.get()
-    format_date = datetime.strptime(selection, '%d.%m.%Y %H:%M:%S')
-    print(format_date)
-    return format_date
-
-
-def cbox_item_select2(event):
-    selection = cbox2.get()
-    format_date = datetime.strptime(selection, '%d.%m.%Y %H:%M:%S')
-    print(format_date)
-    return format_date
-
-
-def result():
-    try:
-        res = cbox_item_select1('') - cbox_item_select2('')
-        label['text'] = round(res.total_seconds())
-        return round(res.total_seconds())
-    except ValueError:
-        msg = 'Не выбраны значения'
-        label["text"] = msg
-
-
-def copy_result():
-    if result():
-        copy = pyperclip.copy(result())
-        label['text'] = 'Данные скопированы'
-        return copy
-    else:
-        label['text'] = 'Нет данных'
-
-
-frame_button = ttk.Frame(root)
-frame_button.pack(expand=0, anchor=CENTER)
-
-btn_result = ttk.Button(frame_button, text='Результат', command=result)
-btn_result.pack()
-
-btn_copy = ttk.Button(frame_button, text='Copy result', command=copy_result)
-btn_copy.pack()
-
-cbox1.bind("<<ComboboxSelected>>", cbox_item_select1)
-cbox2.bind("<<ComboboxSelected>>", cbox_item_select2)
-
-root.mainloop()
+if __name__ == '__main__':
+    app = App()
+    app.mainloop()
